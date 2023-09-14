4fcc6f6e38989b883afcc2c16c9622c1d5c21ee9
PeterDalatu <guruste01@gmail.com>
2023-09-14 13:20:08 -0700

    Update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,34 @@
 #!/usr/bin/node
-function factorial (n) {
-  if (n < 0) {
-    return (-1);
+function computeFactorial(n) {
+  // Base case: factorial of 0 is 1
+  if (isNaN(n) || n < 0) {
+    console.log("Factorial of NaN is 1");
+    return 1;
   }
-  if (n === 0 || isNaN(n)) {
-    return (1);
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * computeFactorial(n - 1);
   }
-  return (n * factorial(n - 1));
 }
 
-console.log(factorial(Number(process.argv[2])));
+function main() {
+  const args = process.argv.slice(2);
+
+  if (args.length !== 1) {
+    console.log("Usage: node factorial.js <integer>");
+    return;
+  }
+
+  const input = parseInt(args[0]);
+
+  if (isNaN(input)) {
+    console.log("Invalid input. Please provide a valid integer.");
+    return;
+  }
+
+  const result = computeFactorial(input);
+  console.log(`Factorial of ${input} is: ${result}`);
+}
+
+main();
